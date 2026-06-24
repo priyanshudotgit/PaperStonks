048a169e1e0c1c48fe11ad7c8e7740a19296f46b
Priyanshu Samanta <priyanshusamanta23@gmail.com>
2026-06-24 19:42:49 +0530

    cors update

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,10 @@ import authRouter from "./routes/auth.routes.js";
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+}));
 app.use(cookieParser());
 
 app.get('/', (req, res) => {
